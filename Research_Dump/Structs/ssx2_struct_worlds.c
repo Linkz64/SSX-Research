1d9de78c0204a82aa4fa2ccabfa5558d401731b3
Linkz <39253822+Linkz64@users.noreply.github.com>
2022-10-12 11:48:13 +0100

    Update ltg

--- Research_Dump/Structs/ssx2_struct_worlds.c
+++ Research_Dump/Structs/ssx2_struct_worlds.c
@@ -63,17 +63,18 @@ struct aip_sop{ // FILE: .AIP and .SOP
 
 struct ltg{ // FILE: .LTG
 
-	struct ltgFile{
-		header     header;
-		offsetList offsetList;
-		bboxData   bboxData;
-	};
+    struct ltgFile{
+        header     header;
+        offsetList offsetList;
+        bboxData   bboxData;
+    };
 
 
-	struct boundingBox{
-		Float32 bounds[3]; * 2   // 2x Vector3's
-		Float32 boundsOrigin[3]; // Middle point of box
-	};
+    struct boundingBox{
+        Float32 bounds[3]; * 2   // 2x Vector3's
+        Float32 boundsOrigin[3]; // Middle point of box
+    };
+
 
     struct header{
         UInt8   NULL; // or 0
@@ -84,58 +85,63 @@ struct ltg{ // FILE: .LTG
         Float32 worldBounds[3]; * 2 // 2x Vector3's | World bounding box (You respawn when you're outside it)
         Float32 worldBoundsOrigin[3]; // Middle point of box
         
-        Float32 gridBoundsSize;   // @ 0x28    = 10000.0
-        UInt32  offsetCount;      // @ 0x2C
-        UInt32  offsetListCount;  // @ 0x30
-        UInt32  Unknown;          // @ 0x34
-        UInt32  mainBoundsCount;  // @ 0x38
-        UInt32  Unknown;          // @ 0x3C
-        Float32 gridBoxSize;      // @ 0x40    = 2500.0
-        UInt32  Unknown;          // @ 0x44    = 4      | Row/Column count
-        UInt32  gridBoxCount;     // @ 0x48    or gridResolution | = 16
-        UInt32  offsetListOffset; // @ 0x4C    = 84
-        UInt32  offsetListEnd;    // @ 0x50    offset of where list data ends (relative to offset 0x00)
+        Float32 gridBoundsSize;    // @ 0x28    = 10000.0
+        UInt32  pointerCount;      // @ 0x2C
+        UInt32  pointerListCount;  // @ 0x30
+        UInt32  Unknown;           // @ 0x34
+        UInt32  mainBoundsCount;   // @ 0x38
+        UInt32  Unknown;           // @ 0x3C
+        Float32 gridBoxSize;       // @ 0x40    = 2500.0
+        UInt32  Unknown;           // @ 0x44    = 4      | Row/Column count
+        UInt32  gridBoxCount;      // @ 0x48    or gridResolution | = 16
+        UInt32  pointerListOffset; // @ 0x4C    = 84
+        UInt32  pointerListEnd;    // @ 0x50    offset of where list data ends (relative to offset 0x00) or list byte size or where box data begins
     };
 
-    struct offsetList{ * offsetListCount
-        UInt32 unkOffset; * offsetCount
+    struct offsetList{ * pointerListCount
+        UInt32 unkOffset; * pointerCount
     };
 
-    struct bboxData{
+    struct bboxData{ // ALL OFFSETS INSIDE ARE RELATIVE TO THE START OF BBOXDATA
         struct mainBounds{
-            boundingBox mainBbox;
-            UInt16 Unknown; // Total Patch count?
-            UInt16 Unknown; // Total Instance count?
+            boundingBox mainBbox; // 9 floats
+            UInt16 Unknown;     // Total Patch count?
+            UInt16 Unknown;     // Total Instance count?
             UInt16 Unknown;
             UInt16 Unknown;
             UInt32 Unknown;
             UInt32 Unknown;
+            UInt32 Unknown; // index list offset (relative to self)
             UInt32 Unknown;
             UInt32 Unknown;
             UInt32 Unknown;
-            UInt32 Unknown;
-            UInt32 Unknown;
+            UInt32 Unknown; // offset leads to list of "00000000 01000000"
             UInt32 Unknown;
         };
         struct gridBounds{
-            boundingBox gridBbox;
+            boundingBox gridBbox;   // 9 floats
             UInt16 patchCount;      // Patch count
             UInt16 instanceCount;   // Instance count
             UInt16 Unknown;         // Instance/Model/Collision count?
             UInt16 splineCount;     // Spline count
             UInt16 lightCount;      // Light count
-            UInt16 Unknown;         // Unknown
-            UInt32 particleCount;   // Particle count (2 UInt16's?)
+            UInt16 extraThingCount; // unused?        (usually 2 but sometimes higher, there's nothing extra to be seen in game)
+            UInt16 particleCount;   // Particle count (a single UInt32?)
+            UInt16 Unknown;         // unused?        (usually 0)
+            UInt32 patchIndexListOffset; // offset leads to it's own index lists
+            UInt32 instanceIndexListOffset;
             UInt32 Unknown;
             UInt32 Unknown;
-            UInt32 Unknown;
-            UInt32 Unknown;
-            UInt32 Unknown;
+            UInt32 extraThingOffset;   // offset leads to it's own extraThing list
             UInt32 Unknown;
         };
 
-        struct extraData{
-            bytes[?] Unknown; // contains indices for patches and maybe instances
+        struct indexList{
+            bytes[?] Unknown; // contains indices for patch, instance, unk, spline, light, unk2, particle, unk3
+        };
+
+        struct extraThing{
+            bytes[8] Unknown;*gridBoxCount // most have just [0, 1] (hex 00000000 01000000)
         };
     };
 };
